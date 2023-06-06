ebb84d5eeee332086a7261baab8eb171960c65bd
Isuru Sanjaya <129564733+djisuru2288@users.noreply.github.com>
2023-06-06 07:20:39 +0530

    Create jscript2.js

---
+++ jscript2.js
@@ -0,0 +1,14 @@
+    function redirectToURL() {
+      var password = document.getElementById("password").value;
+      
+      // Check if the entered password is correct
+      if (password === "0715744120") {
+        setTimeout(function() {
+          window.location.href = "https://prabath556.blogspot.com"; // Redirect to site 1
+        }, 1500);
+      } else {
+        setTimeout(function() {
+          window.location.href = "https://www.google.com"; // Redirect to site 2
+        }, 1500);
+      }
+    }
